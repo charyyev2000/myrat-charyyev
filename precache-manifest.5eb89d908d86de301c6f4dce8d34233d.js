486b6a2c2b44911a6626b29a6a69ca35d1d5dda9
Myrat Charyyev <charyyev2018@mail.ru>
2021-10-25 23:42:13 +0500

    Updates

---
+++ precache-manifest.5eb89d908d86de301c6f4dce8d34233d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fadbc446ee427ca507f8e0d0e0eb7c",
+    "url": "/myrat-charyyev/index.html"
+  },
+  {
+    "revision": "c5d7dac725b9a1a11477",
+    "url": "/myrat-charyyev/static/css/main.814e1514.chunk.css"
+  },
+  {
+    "revision": "824839047c3393ef4b15",
+    "url": "/myrat-charyyev/static/js/2.8f80ad6a.chunk.js"
+  },
+  {
+    "revision": "1cc15aeebc7eb4d75945abeb2f35d703",
+    "url": "/myrat-charyyev/static/js/2.8f80ad6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d7dac725b9a1a11477",
+    "url": "/myrat-charyyev/static/js/main.37df44f3.chunk.js"
+  },
+  {
+    "revision": "7e377e2fa6e3460d75b0699e52d26b1a",
+    "url": "/myrat-charyyev/static/js/main.37df44f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbb08cb0b75472412fc",
+    "url": "/myrat-charyyev/static/js/runtime-main.2f491d13.js"
+  },
+  {
+    "revision": "ef97bb4666ff435a69ab42085f81bc21",
+    "url": "/myrat-charyyev/static/media/codingVector.ef97bb46.png"
+  },
+  {
+    "revision": "4da35bbb28b8689e03167acf8e460cdc",
+    "url": "/myrat-charyyev/static/media/left-arrow.4da35bbb.svg"
+  },
+  {
+    "revision": "64c224f4d2f224353b11f05ad1822c71",
+    "url": "/myrat-charyyev/static/media/right-arrow.64c224f4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myrat-charyyev/precache-manifest.29f66f11f20faa8fbcebe115ed695a5c.js"
+  "/myrat-charyyev/precache-manifest.5eb89d908d86de301c6f4dce8d34233d.js"
 );
 
 self.addEventListener('message', (event) => {
